adebfbafe686876ad6a261e062ab5517e4f0e1cb
antonoeschmidt <s163053@student.dtu.dk>
2019-10-25 11:23:43 +0200

    Test implementeret

--- test/DiceTest.java
+++ test/DiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class DiceTest {
+    private int rolls[] = {0, 0, 0, 0, 0, 0};
 
     @Test
     public void simpleRoll() {
@@ -17,6 +18,36 @@ public class DiceTest {
     @Test
     public void frequencyRoll() {
         Dice dice = new Dice(0);
+        for (int i = 0; i < 6000000; i++) {
+            dice.roll();
+            if (dice.getFaceValue() == 1) {
+                rolls[0]++;
+            } else if (dice.getFaceValue() == 2) {
+                rolls[1]++;
+            } else if (dice.getFaceValue() == 3) {
+                rolls[2]++;
+            } else if (dice.getFaceValue() == 4) {
+                rolls[3]++;
+            } else if (dice.getFaceValue() == 5) {
+                rolls[4]++;
+            } else if (dice.getFaceValue() == 6) {
+                rolls[5]++;
+            }
+
+        }
+        assertEquals(1000000, rolls[0], 5000);
+        assertEquals(1000000, rolls[1], 5000);
+        assertEquals(1000000, rolls[2], 5000);
+        assertEquals(1000000, rolls[3], 5000);
+        assertEquals(1000000, rolls[4], 5000);
+        assertEquals(1000000, rolls[5], 5000);
+        System.out.println("1 : " + rolls[0]);
+        System.out.println("2 : " + rolls[1]);
+        System.out.println("3 : " + rolls[2]);
+        System.out.println("4 : " + rolls[3]);
+        System.out.println("5 : " + rolls[4]);
+        System.out.println("6 : " + rolls[5]);
+
 
     }
 }
