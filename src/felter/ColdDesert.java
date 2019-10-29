77c4598887c97be4da81216c771f467fb5da4e4e
anlc92 <s180646@student.dtu.dk>
2019-10-29 11:16:19 +0100

    prøver lige noget

--- src/felter/ColdDesert.java
+++ src/felter/ColdDesert.java
@@ -4,6 +4,6 @@ public class ColdDesert extends FeltBase {
     public ColdDesert() {
         super.feltVaerdi = -20;
         super.feltNavn = "Cold Desert";
-        super.feltBesked = "Du er i en kold ørken. Du fryser.";
+        super.feltBesked = "Du er i en kold ørken. Du fryser og taber 20 mønter.";
     }
 }
