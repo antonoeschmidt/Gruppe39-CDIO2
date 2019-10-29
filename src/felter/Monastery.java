2d9a2cc0692fc4eedd89786aeb323f60d395921b
Bamse <bbandresen@hotmail.com>
2019-10-29 11:55:53 +0100

    Felt monastery

---
+++ src/felter/Monastery.java
@@ -0,0 +1,9 @@
+package felter;
+
+public class Monastery extends FeltBase {
+    public Monastery() {
+        super.feltVaerdi = 0;
+        super.feltNavn = "Monastery";
+        super.feltBesked = " Du finder en guldmønt på gaden men ser en fattig munk så du giver den til ham så du får ingenting";
+    }
+}
