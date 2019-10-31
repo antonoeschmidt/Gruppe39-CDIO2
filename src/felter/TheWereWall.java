b109ec8a572dcd499e16ccc6c144623fbabff96c
anlc92 <s180646@student.dtu.dk>
2019-10-31 23:02:00 +0100

    lille ændring i TheWereWall felt prøver at lave ekstrea tur

--- src/felter/TheWereWall.java
+++ src/felter/TheWereWall.java
@@ -5,5 +5,6 @@ public class TheWereWall extends FeltBase {
         super.feltVaerdi = -80;
         super.feltNavn = "The WereWall";
         super.feltBesked = "du betaler 80 mønter for at komme igennem porten og spare tid (Du for et ekstra slag)";
+        super.ekstraTur = true;
     }
 }
