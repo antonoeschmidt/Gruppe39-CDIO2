b107e107f1ed7d84bc9264730ea661d7337af899
Sid Ali <sidali-m@hotmail.com>
2019-11-01 10:30:12 +0100

    Merge

--- src/Game.java
+++ src/Game.java
@@ -1,60 +1,145 @@
+import felter.ColdDesert;
+import felter.FeltBase;
+import felter.PalaceGates;
+import felter.Tower;
+import felter.Monastery;
+import felter.WalledCity;
+import felter.Crater;
+import felter.TheWereWall;
+import felter.ThePit;
+import felter.BlackCave;
+import felter.Goldmine;
+import felter.HutsInTheMountain;
+
+
 import java.util.Scanner;
 
 public class Game {
-    private Player player1;
-    private Player player2;
-    private Dice dice1;
-    private Dice dice2;
-    private boolean vind = false;
+    private Scanner in;
 
-
-    public Game(Player player1, Player player2, Dice dice1, Dice dice2) {
-        this.player1 = player1;
-        this.player2 = player2;
-        this.dice1 = dice1;
-        this.dice2 = dice2;
-
+    public Game(){
+        this.in = new Scanner(System.in);
     }
 
-    public void StartGame() {
-        while (vind == false) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Tryk enter for at slå for " + player1.getName());
-            scanner.nextLine();
-            playerTurn(player1);
-            System.out.println("Tryk enter for at slå for " + player2.getName());
-            scanner.nextLine();
-            playerTurn(player2);
-            System.out.println(player1.getName() + " score: " + player1.getScore());
-            System.out.println(player2.getName() + " score: " + player2.getScore());
+    private void pauseNaesteRunde(boolean ekstraTur){
+        if (ekstraTur){
+            System.out.print("Tryk ENTER for at spille ekstratur...");
+        } else {
+            System.out.print("Tryk ENTER for næste runde...");
         }
-        if (player1.isWinner() ) {
-            System.out.println(player1.getName() + " vinder med " + player1.getScore() + " point.");
+        in.nextLine();
+    }
+
+    public void startSpil() {
+        int terning1Min = 1;
+        int terning1Max = 6;
+        Terning terning1 = new Terning(terning1Min, terning1Max);
+
+        int terning2Min = 1;
+        int terning2Max = 6;
+        Terning terning2 = new Terning(terning2Min, terning2Max);
+
+        SpilleBraet braet = new SpilleBraet(terning1, terning2);
+
+        braet.addFelt(new PalaceGates());
+        braet.addFelt(new Tower());
+        braet.addFelt(new ColdDesert());
+        braet.addFelt(new Monastery());
+        braet.addFelt(new WalledCity());
+        braet.addFelt(new Crater());
+        braet.addFelt(new ThePit());
+        braet.addFelt(new TheWereWall());
+        braet.addFelt(new HutsInTheMountain());
+        braet.addFelt(new Goldmine());
+        braet.addFelt(new BlackCave());
+
+        String name1, name2;
+        int win_score = 3000;
+
+        System.out.print("Indtast navn på spiller 1: ");
+        name1 = this.in.nextLine();
+        System.out.print("indtast navn på spiller 2: ");
+        name2 = this.in.nextLine();
+
+        System.out.println();
+
+        System.out.println("player1: " + name1 + ", " + "player2: " + name2);
+
+        Spiller spiller1 = new Spiller(name1);
+        Spiller spiller2 = new Spiller(name2);
+
+        Spiller[] spillere = {spiller1, spiller2};
+
+        System.out.println();
+
+        this.pauseNaesteRunde(false);
+
+        boolean spilErSlut = false;
+
+        while (!spilErSlut) {
+            Spiller spillerHoejsteScore = null;
+
+            for (Spiller spiller : spillere) {
+                System.out.println();
+
+                Konto spillerKonto = spiller.getKonto();
+
+                // Vis nuværende score for begge spillere inden der rulles med terningerne
+                System.out.println(spiller.getNavn() + " balance: " + spillerKonto.getBalance());
+
+                this.spilRunde(braet, terning1, terning2, spillerKonto);
+
+                if (spillerHoejsteScore == null) {
+                    spillerHoejsteScore = spiller;
+                } else {
+                    if (spillerKonto.getBalance() > spillerHoejsteScore.getKonto().getBalance()) {
+                        spillerHoejsteScore = spiller;
+                    }
+                }
+
+
+            }
+
+            if (spillerHoejsteScore.getKonto().getBalance() >= win_score) {
+                spilErSlut = true;
+
+                System.out.println();
+
+                System.out.println(spillerHoejsteScore.getNavn() + " har vundet med " + spillerHoejsteScore.getKonto().getBalance() + " points!");
+            }
         }
-        else if (player2.isWinner()){
-            System.out.println(player2.getName() + " vinder med " + player2.getScore() + " point.");
+    }
+    private void spilRunde(SpilleBraet braet, Terning terning1, Terning terning2, Konto spillerKonto) {
+        int number1 = terning1.roll();
+        int number2 = terning2.roll();
+
+        int sum = number1 + number2;
+        System.out.println("resultat: " + number1 + " + " + number2 + " = " + sum);
+
+        FeltBase felt = braet.getFelt(sum);
+
+        int feltVaerdi = felt.getVaerdi();
+
+        System.out.println(felt.getNavn());
+        System.out.println(felt.getBesked());
+
+        if (feltVaerdi > 0) {
+            spillerKonto.deposit(feltVaerdi);
+        } else {
+            // Brug den absolutte værdi (realværdien), så f.eks. -300 eller 300 bliver til 300
+            spillerKonto.withdraw(Math.abs(feltVaerdi));
         }
-        }
 
+        System.out.println("Ny score: " + spillerKonto.getBalance());
 
-    private void playerTurn(Player player) {
+        // Pause med forskellig besked alt efter om det er en ekstra tur
+        boolean rundeErEkstraTur = felt.getEkstraTur();
+        this.pauseNaesteRunde(rundeErEkstraTur);
 
-        int [] gameRules;
-        gameRules = new int[] {250,-100,100,-20,180,0,-70,60,-80,-50,650};
+        if (felt.getEkstraTur()) {
+            System.out.println();
+            this.spilRunde(braet, terning1, terning2, spillerKonto);
 
-
-        dice1.roll();
-        dice2.roll();
-        player.setScore(player.getScore() + dice1.getFaceValue());
-        System.out.println(player.getName() + " slår med første terning = " + dice1.getFaceValue());
-        player.setScore(player.getScore() + dice2.getFaceValue());
-        System.out.println(player.getName() + " slår med anden terning = " + dice2.getFaceValue());
-
-        player.setScore(player.getScore() + gameRules[dice1.getFaceValue() + dice2.getFaceValue()]);
-
-        if (player.getScore() >= 40 && dice1.getFaceValue() == dice2.getFaceValue()){
-            vind = true;
-            player.setWinner(true);
         }
     }
 
